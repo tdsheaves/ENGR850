830666c410ec8fd58af5512715802756c8266355
Tyler Sheaves <tdsheaves@yahoo.com>
2020-01-30 13:24:06 -0800

    Update

--- Data_Types/data_types.sv
+++ Data_Types/data_types.sv
@@ -11,12 +11,12 @@ module SV_Types();
 		two_byte_unsigned   = 16'hffff;
 		single_signed_byte  = -127;
 		#1;
-		$display(  "  bit Value: %0b\
-				  \n2byte Value: %0h\
-				  \n1byte Value: %0d",
-				   single_bit_unsigned,
-				   two_byte_unsigned,
-				   single_signed_byte);
+		$display(  "  bit Value: %0b => %0b\
+				  \n2byte Value: %0h => %0b\
+				  \n1byte Value: %0d => %0b",
+				   single_bit_unsigned, single_bit_unsigned, 
+				   two_byte_unsigned, two_byte_unsigned,
+				   single_signed_byte, single_signed_byte);
 
 	end
 
